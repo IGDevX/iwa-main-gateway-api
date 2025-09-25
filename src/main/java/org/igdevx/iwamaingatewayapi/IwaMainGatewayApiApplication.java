69847f2911c845ba5129bc2e9d11e3dc7b8fbe60
Alexandre-Roussel48 <alexandre.roussel.48@gmail.com>
2025-09-25 11:56:44 +0200

    chore: initial boilerplate for gateway api

---
+++ src/main/java/org/igdevx/iwamaingatewayapi/IwaMainGatewayApiApplication.java
@@ -0,0 +1,13 @@
+package org.igdevx.iwamaingatewayapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwaMainGatewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwaMainGatewayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/igdevx/iwamaingatewayapi/IwaMainGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.igdevx.iwamaingatewayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwaMainGatewayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
