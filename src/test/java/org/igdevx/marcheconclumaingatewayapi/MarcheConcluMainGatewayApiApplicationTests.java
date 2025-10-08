1ae341924485ac1c7dcc2b3087a5115cedb1d2d0
Alexandre-Roussel48 <alexandre.roussel.48@gmail.com>
2025-10-08 11:45:47 +0200

    chore: renamed gateway to include project name

--- src/test/java/org/igdevx/marcheconclumaingatewayapi/MarcheConcluMainGatewayApiApplicationTests.java
+++ src/test/java/org/igdevx/marcheconclumaingatewayapi/MarcheConcluMainGatewayApiApplicationTests.java
@@ -1,10 +1,10 @@
-package org.igdevx.iwamaingatewayapi;
+package org.igdevx.marcheconclumaingatewayapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IwaMainGatewayApiApplicationTests {
+class MarcheConcluMainGatewayApiApplicationTests {
 
 	@Test
 	void contextLoads() {
